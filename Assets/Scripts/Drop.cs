4cc9a983d339e8d1db3ef780d98840bed0605622
Xillez <kentwh@stud.ntnu.no>
2019-01-12 12:59:45 +0100

    Added some files for terrain erosion algorith,

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop
+{
+	public Vector3 position; 
+	public float radius;
+}
---
+++ Assets/Scripts/FluidErosionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidErosionManager : MonoBehaviour
+{
+	List<Drop> drops;
+	public int nrParticles;
+	public float sedimentCarryAmount;
+
+	void Start()
+	{
+		//
+	}
+	
+	void Update()
+	{
+		//
+	}
+}
