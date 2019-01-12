29fd2d124fe96d548a2fc791f8957b67c48f11cb
Xillez <kentwh@stud.ntnu.no>
2019-01-12 16:38:36 +0100

    Added oncstructor in Drop class, started the setup for the algorithm and other minor changes

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -5,5 +5,15 @@ using UnityEngine;
 public class Drop
 {
 	public Vector3 position; 
+	public Vector3 direction;
+	public float velocity;
 	public float radius;
+
+	public Drop(Vector3 position, Vector3 direction, float velocity, float radius)
+	{
+		this.position = position;
+		this.direction = direction;
+		this.velocity = velocity;
+		this.radius = radius;
+	}
 }
--- Assets/Scripts/FluidErosionManager.cs
+++ Assets/Scripts/FluidErosionManager.cs
@@ -5,24 +5,55 @@ using UnityEngine;
 public class FluidErosionManager : MonoBehaviour
 {
 	public TerrainData terrain;
-	List<Drop> drops;
+	List<Drop> drops = new List<Drop>();
 	// ---- Drop settings
-	public int nrDrops;
+	Bounds terrainBounds;
+	public float rainDensity;
 	public float dropRadius;
 	public float sedimentCarryAmount;
 
 	// ---- Terrain settings
 	public float sedimentWeight;
-	public float sedimentRegidety;
+	public float sedimentRigidity;
 
 
 	void Start()
 	{
-		//
+		if (this.terrain == null)
+		{
+			Debug.Log("No terrain data specified!");
+			return;
+		}
+
+		if (this.rainDensity <= 0.0f)
+		{
+			Debug.Log("Rain density can't be 0 or under!");
+			return;
+		}
+		
+		// Get bounds of terrain
+		this.terrainBounds = terrain.bounds;
+
+		// Calculate horizontal seperation
+		Vector3 separation = new Vector3(this.terrainBounds.size.x / (this.dropRadius / this.rainDensity), 0, this.terrainBounds.size.z / (this.dropRadius / this.rainDensity));
+
+		// Place all rain drops
+		for (int z = 0; z < (this.terrainBounds.size.z / separation.z) + 1; ++z)
+		{
+			for (int x = 0; x < (this.terrainBounds.size.x / separation.x) + 1; ++x)
+			{
+				this.drops.Add(new Drop(
+						new Vector3(terrainBounds.min.x + (separation.x * x), 0, terrainBounds.min.z + (separation.z * z)), Vector3.down, 1.0f, this.dropRadius
+					)
+				);
+			}
+		}
 	}
 	
 	void Update()
 	{
 		//
 	}
+
+	
 }
