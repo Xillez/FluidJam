f44c722ab5928394bb4fb060a01e6ed323fbe499
Xillez <kent.wincent46@gmail.com>
2019-01-13 09:51:48 +0100

    Reduced drop radius and some other algorithmic changes

--- Assets/Scripts/FluidErosionManager.cs
+++ Assets/Scripts/FluidErosionManager.cs
@@ -65,15 +65,16 @@ public class FluidErosionManager : MonoBehaviour
         //{
         for (int drop = 0; drop < this.drops.Count; ++drop)
         {
-            Vector2 normalizedTerrainPosition = new Vector2(this.terrainBounds.size.x / (this.drops[drop].position.x - this.terrainBounds.min.x), this.terrainBounds.size.z / (this.drops[drop].position.z - this.terrainBounds.min.z));
+            Vector2 normalizedTerrainPosition = new Vector2((this.drops[drop].position.x - this.terrainBounds.min.x) / this.terrainBounds.size.x, (this.drops[drop].position.z - this.terrainBounds.min.z) / this.terrainBounds.size.z);
             this.drops[drop].velocity = terrain.terrainData.GetSteepness(normalizedTerrainPosition.x, normalizedTerrainPosition.y);
+            Debug.Log("Velocity: " + this.drops[drop].velocity);
             Vector3 normalAtDropPos = terrain.terrainData.GetInterpolatedNormal(normalizedTerrainPosition.x, normalizedTerrainPosition.y);
             this.drops[drop].direction = Vector3.Cross(normalAtDropPos, -Vector3.Cross(Vector3.up, normalAtDropPos));
             //Debug.DrawLine(this.drops[drop].position, /*this.drops[drop].position +*/ this.drops[drop].direction * this.drops[drop].velocity);
 
 
-           /* this.drops[drop].position += this.drops[drop].direction * this.drops[drop].velocity;// * Time.deltaTime;
-            this.drops[drop].position.y = terrain.SampleHeight(this.drops[drop].position);*/
+            this.drops[drop].position += this.drops[drop].direction * this.drops[drop].velocity;// * Time.deltaTime;
+            this.drops[drop].position.y = terrain.SampleHeight(this.drops[drop].position);
         }
         //}
 	}
@@ -82,10 +83,10 @@ public class FluidErosionManager : MonoBehaviour
 	{
         if (this.drops != null)
         {
-            for (int i = 0; i < this.drops.Count / 8.0f; ++i)
+            for (int i = 0; i < this.drops.Count / 16.0f; ++i)
             {
-                //Gizmos.DrawSphere(this.drops[i].position, this.drops[i].radius);
-                Debug.DrawLine(this.drops[i].position, /*this.drops[i].position + */this.drops[i].direction * this.drops[i].velocity);
+                Gizmos.DrawSphere(this.drops[i].position, this.drops[i].radius);
+                //Debug.DrawLine(this.drops[i].position, /*this.drops[i].position + */this.drops[i].direction * this.drops[i].velocity);
             }
         }
 	}
